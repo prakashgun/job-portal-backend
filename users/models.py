4fdcbf06bc9ad95ff22910cfcf534a9af5d6308b
prakashgun <mail.prakash.india@gmail.com>
2019-07-05 19:34:38 +0530

    Added employee

--- users/models.py
+++ users/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
